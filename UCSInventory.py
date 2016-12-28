34371b3695be009db490fb535b69e9352eabfabc
Yang <jyang3@activenetwork.com>
2016-12-28 11:35:01 +0800

    update

--- UCSInventory.py
+++ UCSInventory.py
@@ -34,32 +34,31 @@ for blade in query_data['ComputeBlade']:
     query_dict['blade'][blade.dn]['model'] = blade.model
     query_dict['blade'][blade.dn]['serial'] = blade.serial
     query_dict['blade'][blade.dn]['available_memory'] = blade.available_memory
-    #print blade.dn
+    # print blade.dn
     blade_rn = blade.dn.split('/')
-    blade_parent = blade_rn[0]+'/'+blade_rn[1]
+    blade_parent = blade_rn[0] + '/' + blade_rn[1]
     blade_parent_info = handle.query_dn(blade_parent)
-    #print blade_parent_info.dn
-    blade_parent_chindren = handle.query_children(in_dn=blade_parent_info.dn, class_id="EquipmentIOCard")
+    # print blade_parent_info.dn
+    blade_parent_chindren = handle.query_children(
+        in_dn=blade_parent_info.dn, class_id="EquipmentIOCard")
 
-    #print blade_parent_chindren[0].model, blade_parent_chindren
+    # print blade_parent_chindren[0].model, blade_parent_chindren
     query_dict['blade'][blade.dn]['IOM'] = blade_parent_chindren[0].model
-    #print query_dict['IOM']
+    # print query_dict['IOM']
     blade_info = handle.query_dn(blade.dn)
-    blade_info_chindren = handle.query_children(in_mo=blade_info, class_id="adaptorUnit")
-    #print blade_info_chindren
+    blade_info_chindren = handle.query_children(
+        in_mo=blade_info, class_id="adaptorUnit")
+    # print blade_info_chindren
     for blade_child_object in blade_info_chindren:
         query_dict['blade'][blade.dn]['adaptor'] = blade_child_object.model
 
+    # print blade_parent
 
 
-    #print blade_parent
-
-
-
-#print query_dict.keys()
-#print query_dict["IOM"]
+# print query_dict.keys()
+# print query_dict["IOM"]
 print query_dict
-#print blade
+# print blade
 
 
 handle.logout()
